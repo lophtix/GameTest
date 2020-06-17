b5b2ecf810b160a30bbd8a01b1cf4167dcf60ff3
Jonatan Wästlund <jonte.w1@gmail.com>
2020-06-17 14:44:48 +0200

    Lobby!
    There is now a lobby to which you can add clients. They can however not be removed yet.

--- SummerGame/Scripts/Menu.gd
+++ SummerGame/Scripts/Menu.gd
@@ -2,65 +2,69 @@ extends Node
 
 export (String) var scene
 
-var check
-var network
+var player_location = "res://Prefabs/Player.tscn" 
+var connection
+var lobby
 
-var player_location = "res://Prefabs/Player.tscn"
+var check
+var players
+var player_name
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.connection = get_node("Connection")
+	self.lobby = get_node("Lobby")
+	self.players = {}
 
 func start_client():
 	self.check = true
-	self.network = NetworkedMultiplayerENet.new()
-	var ip = get_child(0).get_child(0).get_child(0).text as String
-	var port = get_child(0).get_child(0).get_child(1).text as int
-	self.network.create_client(ip, port)
-	get_tree().set_network_peer(self.network)
+	var peer = NetworkedMultiplayerENet.new()
+	var ip = get_node("Connection").get_node("Address").get_node("IPAddress").text as String
+	var port = get_node("Connection").get_node("Address").get_node("Port").text as int
+	self.player_name = get_node("Connection").get_node("Name").text
+	peer.create_client(ip, port)
+	get_tree().set_network_peer(peer)
+	update_lobby()
 
-	self.get_child(0).hide()
-	var gameScene = load(scene).instance()
-	get_tree().get_root().add_child(gameScene)
-
-	var player = load(player_location).instance()
-	player.set_network_master(get_tree().get_network_unique_id())
-	get_tree().get_root().get_node("Main").add_child(player)
+	self.connection.hide()
+	self.lobby.show()
 
 func start_server():
 	self.check = false
-	self.network = NetworkedMultiplayerENet.new()
-	var ip = get_child(0).get_child(0).get_child(0).text as String
-	var port = get_child(0).get_child(0).get_child(1).text as int
-	self.network.create_server(port, 12)
-	get_tree().set_network_peer(self.network) 
-	
-	self.get_child(0).hide()
-	var gameScene = load(scene).instance()
-	get_tree().get_root().add_child(gameScene)
+	var peer = NetworkedMultiplayerENet.new()
+	var port = get_node("Connection").get_node("Address").get_node("Port").text as int
+	self.player_name = get_node("Connection").get_node("Name").text
+	peer.create_server(port, 12)
+	get_tree().set_network_peer(peer)
+	self.players[get_tree().get_network_unique_id()] = self.player_name
+	update_lobby()
 
-	var player = load(player_location).instance()
-	player.set_network_master(get_tree().get_network_unique_id())
-	get_tree().get_root().get_node("Main").add_child(player)
+	self.connection.hide()
+	self.lobby.show()
 
-remote func player_server():
+remote func register_player(register_name):
 	var id = get_tree().get_rpc_sender_id()
-	print("Server: " + id as String)
-	var player = load(player_location).instance()
-	player.set_network_master(id)
-	get_tree().get_root().get_node("Main").add_child(player)
-	rpc_id(id, "player")
+	print(register_name)
+	self.players[id] = register_name as String
+	for player in self.players:
+		rpc("update_players",self.players[player], player)
+	update_lobby()
 
-remote func player():
-	var id = get_tree().get_rpc_sender_id()
-	print("client: " + id as String)
-	var player = load(player_location).instance()
-	player.set_network_master(id)
-	get_tree().get_root().get_node("Main").add_child(player)
+remote func update_players(register_name, id):
+	self.players[id] = register_name
+	update_lobby()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func update_lobby():
+	for child in self.lobby.get_node("players").get_children():
+		child.queue_free()
+
+	for player in self.players:
+		var listed_player = Label.new()
+		listed_player.text = self.players[player]
+		self.lobby.get_node("players").add_child(listed_player)
+
 func _process(delta):
-	if (check):
-		print(self.network.get_connection_status())
-		if (self.network.get_connection_status() == 2):
-			rpc("player_server")
+	if (self.check):
+		if (get_tree().network_peer.get_connection_status() == 2):
 			self.check = false
+			rpc("register_player", self.player_name)
